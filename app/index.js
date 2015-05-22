8a2c5b5cbca6f6b91c77f93da8bdf3db6a92f18b
Thiago Poiani <thpoiani@gmail.com>
2015-05-22 16:50:16 -0300

    Recuperação de URL de abril e maio

--- app/index.js
+++ app/index.js
@@ -5,9 +5,52 @@ var constants = require('./config/consts'),
     fs        = require('fs');
 
 page.open(constants.OSM_LISTINFO, function(status) {
-  if(status === "success") {
+  if(status !== "success") {
     console.error("Erro ao acessar: " + constants.OSM_LISTINFO)
+    return phantom.exit();
   }
+
+  var MailingList = (function() {
+    var exports = {},
+        listinfo,
+        pipermail;
+
+    function getInfoLists() {
+      return page.evaluate(function() {
+        var matches = document.querySelectorAll('a[href^=listinfo]'),
+            nodes   = [];
+
+        for (var i = 0; i < matches.length; i++) {
+          nodes.push(matches[i].href);
+        };
+
+        return nodes;
+      });
+    }
+
+    function getPiperMails() {
+      return getInfoLists().map(function(info) {
+        return info.replace('listinfo', 'pipermail');
+      });
+    }
+
+    exports.getThread = function (month, year) {
+      return getPiperMails().map(function(mail) {
+        return mail += '/' + year + '-' + month + '/thread.html';
+      });
+    }
+
+    exports.title = page.evaluate(function() {
+        return document.title;
+    });
+
+    return exports;
+  })();
+
+  console.log(MailingList.title);
+
+  var april = MailingList.getThread('April', 2015),
+      may   = MailingList.getThread('May', 2015);
 
   phantom.exit();
 });
