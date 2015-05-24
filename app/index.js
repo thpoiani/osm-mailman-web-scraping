c31503fb66f74fda709e00b24a7c9221afbf08ea
Thiago Poiani <thpoiani@gmail.com>
2015-05-24 01:01:34 -0300

    Coleta de emails

--- app/index.js
+++ app/index.js
@@ -1,56 +1,12 @@
 'use strict';
 
-var constants = require('./config/consts'),
-    page      = require('webpage').create(),
-    fs        = require('fs');
+var Mailing  = require('./MailingList.js'),
+    LISTINFO = 'https://lists.openstreetmap.org/listinfo',
+    MONTH    = 'May',
+    YEAR     = 2015;
 
-page.open(constants.OSM_LISTINFO, function(status) {
-  if(status !== "success") {
-    console.error("Erro ao acessar: " + constants.OSM_LISTINFO)
-    return phantom.exit();
-  }
+Mailing(LISTINFO, YEAR, MONTH, function(subjects) {
 
-  var MailingList = (function() {
-    var exports = {},
-        listinfo,
-        pipermail;
+  console.log(JSON.stringify(subjects));
 
-    function getInfoLists() {
-      return page.evaluate(function() {
-        var matches = document.querySelectorAll('a[href^=listinfo]'),
-            nodes   = [];
-
-        for (var i = 0; i < matches.length; i++) {
-          nodes.push(matches[i].href);
-        };
-
-        return nodes;
-      });
-    }
-
-    function getPiperMails() {
-      return getInfoLists().map(function(info) {
-        return info.replace('listinfo', 'pipermail');
-      });
-    }
-
-    exports.getThread = function (month, year) {
-      return getPiperMails().map(function(mail) {
-        return mail += '/' + year + '-' + month + '/thread.html';
-      });
-    }
-
-    exports.title = page.evaluate(function() {
-        return document.title;
-    });
-
-    return exports;
-  })();
-
-  console.log(MailingList.title);
-
-  var april = MailingList.getThread('April', 2015),
-      may   = MailingList.getThread('May', 2015);
-
-  phantom.exit();
 });
